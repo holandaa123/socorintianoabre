4db6dcb4a98c91449096826a1d58c674c135902b
holandaa123 <131389615+holandaa123@users.noreply.github.com>
2023-07-14 13:22:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let time = 2000,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll("#slider img")
+    max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+function contato(){
+    window.location
+    console.log('### window', window)
+    console.log('### window.location', window.location)
+}
+window.addEventListener("load", start)
+
+
